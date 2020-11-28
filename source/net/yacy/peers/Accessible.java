53d156768e9168d18922b3b0a48d99b96b86154b
A984382 <72402052+A984382@users.noreply.github.com>
2020-11-28 09:25:46 +0100

    Update Accessible.java

--- source/net/yacy/peers/Accessible.java
+++ source/net/yacy/peers/Accessible.java
@@ -49,9 +49,9 @@ public class Accessible {
         	final File shortcut = new File(sb.getAppPath() + "/addon/YaCy-Search.html".replace("/", File.separator));
         	final String content = "<meta http-equiv=\"refresh\" content=\"0;url=http://localhost:" + newPort + "/\">";
         	FileUtils.copy(UTF8.getBytes(content), shortcut);
-		} catch (final Exception e) {
-			return;
-		}
+	} catch (final Exception e) {
+		return;
+	}
     }
     
     /**
@@ -64,8 +64,8 @@ public class Accessible {
         	final File shortcut = new File(sb.getAppPath() + "/addon/YaCy-Search.bat".replace("/", File.separator));
         	final String content = "rundll32 url.dll,FileProtocolHandler \"http://localhost:" + newPort + "\"";
         	FileUtils.copy(UTF8.getBytes(content), shortcut);
-		} catch (final Exception e) {
-			return;
-		}
+	} catch (final Exception e) {
+		return;
+	}
     }
 }
